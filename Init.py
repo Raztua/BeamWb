9bdf1dcfdf7d0d2295bf8a726241e00a6c03d22e
Syres916 <46537884+Syres916@users.noreply.github.com>
2026-02-02 15:59:24 +0000

    Update Init.py

--- Init.py
+++ Init.py
@@ -1,13 +1,20 @@
-class BeamWB:
-    def __init__(self):
-        pass
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2026                                                    *
+# *                                                                         *
+# *   This program is free software: you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License as        *
+# *   published by the Free Software Foundation, either version 3 of the    *
+# *   License, or (at your option) any later version.                       *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Lesser General Public License for more details.                   *
+# *                                                                         *
+# *   You should have received a copy of the GNU Lesser General Public      *
+# *   License along with this program.  If not,                             *
+# *   see <https://www.gnu.org/licenses/>.                                  *
+# *                                                                         *
+# ***************************************************************************/
 
-    def Initialize(self):
-        # This will be called when the workbench is loaded
-        pass
-
-    def GetClassName(self):
-        return "Gui::PythonWorkbench"
-
-# The workbench will be created when FreeCAD starts
-FreeCAD.addImportType("Beam Workbench", "*")
